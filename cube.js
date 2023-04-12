e05896adca1564f073b94f894871765576ff746c
renie.velasquez <renie.velasquez@futureanalytics.io>
2023-04-12 14:40:52 +0000

    ab

--- cube.js
+++ cube.js
@@ -7,3 +7,19 @@
 
 module.exports = {
 };
+
+
+/*
+Total Sales Amount = sum(FactInternetSales[SalesAmount])
+Total Quantity = sum(FactInternetSales[OrderQuantity])
+Average Unit Price = DIVIDE([Total Sales Amount],[Total Quantity],blank())
+
+Total Sales Amount - LY = CALCULATE([Total Sales Amount],SAMEPERIODLASTYEAR(DimDate[FullDateAlternateKey]))
+Total Quantity - LY = CALCULATE([Total Quantity],SAMEPERIODLASTYEAR(DimDate[FullDateAlternateKey]))
+Average Unit Price - LY = CALCULATE([Average Unit Price],SAMEPERIODLASTYEAR(DimDate[FullDateAlternateKey])
+
+
+PVM Price(Rev) = [Total Quantity - LY] * ([Average Unit Price]-[Average Unit Price - LY])
+PVM Vol(Rev) = [Average Unit Price - LY]*([Total Quantity]-[Total Quantity - LY])
+PVM Mix(Rev) = ([Average Unit Price]-[Average Unit Price - LY])*([Total Quantity]-[Total Quantity - LY])
+*/
